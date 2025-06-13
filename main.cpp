6ff396ae14bc22d62312e3093ecd1e317c9e719b
oldleee <2437827956@qq.com>
2025-06-13 16:41:26 +0800

    在main.cpp中添加冒泡排序函数

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 using namespace std;
 
+void PubbleSort(std::vector<int>& v, int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		for (int j = 0; j < size - 1 - i; ++j) {
+			if (v[j] > v[j + 1]) {
+				swap(v[j], v[j + 1]);
+			}
+		}
+	}
+}
+
 int main() {
 	std::vector<int> v{ 12,2,4,122 };
 	std::cout << "before sort:"
