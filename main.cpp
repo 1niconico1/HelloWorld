3f0e6e3b1b3732f06a2be3e3873f642747417482
oldleee <2437827956@qq.com>
2025-06-13 19:12:27 +0800

    增加快速排序算法

--- main.cpp
+++ main.cpp
@@ -12,14 +12,44 @@ void PubbleSort(std::vector<int>& v, int size) {
 	}
 }
 
+void QuickSort(std::vector<int>& v, int left, int right){
+	if(left > right){
+		return ;
+	}
+	
+	int pivot_idx = rand()%(right - left) + left;
+	int pivot = v[pivot_idx];
+
+	int i = left, j = right;
+	while(i <=j){
+		while(i <= j && v[i] < pivot){
+			++i;
+		}
+
+		while(i <= j && v[j] > pivot){
+			--j;
+		}
+		
+		if(i <= j){
+			swap(v[i], v[j]);
+			++i;
+			--j;
+		}
+	}
+
+	QuickSort(v, left, j);
+	QuickSort(v, j + 1, right);
+	
+}
 int main() {
+	srand(time(nullptr));
 	std::vector<int> v{ 12,2,4,122 };
 	std::cout << "before sort:"
 	for (int x : v) {
 		std::cout << x << " " << ;
 	}
 	std::cout << std::endl;
-	std::sort(v.begin(), v.end());
+	QuickSort(v, 0, v.size());
 	std::cout << "after sort:"
 	for (int x : v) {
 		std::cout << x << " " << ;
